9c177ead9b38515be9f56c399aab71513334609a
Yaniv Schweitzer <yaniv10501@gmail.com>
2022-01-08 00:18:40 +0200

    saved articles not found error

--- src/components/SavedCardList/SavedCardList.js
+++ src/components/SavedCardList/SavedCardList.js
@@ -7,7 +7,7 @@ import NewsCard from '../NewsCard/NewsCard';
 import Preloader from '../Preloader/Preloader';
 import handleImageLoad from '../../utils/handleImageLoad';
 
-function SavedCardList({ result, thunkDispatch }) {
+function SavedCardList({ result, error, thunkDispatch }) {
   let loadingImages = [];
   const [isLoadingMore, setIsLoadingMore] = useState(false);
   const [isShowMoreVisible, setIsShowMoreVisible] = useState(testArticles.length > 3);
@@ -41,61 +41,71 @@ function SavedCardList({ result, thunkDispatch }) {
   }, [result]);
   return (
     <section className="news-card-list">
-      <ul className="news-card-list__grid">
-        {result &&
-          result.length > 0 &&
-          result.map((article, index) =>
-            index > cardAmount ? (
-              ''
-            ) : (
-              // eslint-disable-next-line react/no-array-index-key
-              <NewsCard article={article} key={index} handleImageLoad={handleImageLoading}>
-                <div className="news-card__keyword">{article.keyword}</div>
-                <div
-                  className="news-card__delete-button"
-                  id={article._id}
-                  onClick={handleDeleteClick}
-                  onKeyDown={() => {}}
-                  role="button"
-                  tabIndex={0}
-                >
-                  <div className="news-card__delete-text">Remove from saved</div>
-                </div>
-              </NewsCard>
-            )
-          )}
-      </ul>
-      <div
-        className={
-          isShowMoreVisible
-            ? 'news-card-list__button-overlay'
-            : 'news-card-list__button-overlay news-card-list__button-overlay_hidden'
-        }
-      >
-        <Preloader isLoading={isLoadingMore} />
-        <button
-          className={
-            isLoadingMore
-              ? 'news-card-list__show-more-button news-card-list__show-more-button_hidden'
-              : 'news-card-list__show-more-button'
-          }
-          type="button"
-          onClick={handleShowMoreClick}
-        >
-          Show more
-        </button>
-      </div>
+      {error ? (
+        <div className="news-card-list__get-container">
+          <h2 className="news-card-list__get-title">Search for articles</h2>
+        </div>
+      ) : (
+        <>
+          <ul className="news-card-list__grid">
+            {result &&
+              result.length > 0 &&
+              result.map((article, index) =>
+                index > cardAmount ? (
+                  ''
+                ) : (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <NewsCard article={article} key={index} handleImageLoad={handleImageLoading}>
+                    <div className="news-card__keyword">{article.keyword}</div>
+                    <div
+                      className="news-card__delete-button"
+                      id={article._id}
+                      onClick={handleDeleteClick}
+                      onKeyDown={() => {}}
+                      role="button"
+                      tabIndex={0}
+                    >
+                      <div className="news-card__delete-text">Remove from saved</div>
+                    </div>
+                  </NewsCard>
+                )
+              )}
+          </ul>
+          <div
+            className={
+              isShowMoreVisible
+                ? 'news-card-list__button-overlay'
+                : 'news-card-list__button-overlay news-card-list__button-overlay_hidden'
+            }
+          >
+            <Preloader isLoading={isLoadingMore} />
+            <button
+              className={
+                isLoadingMore
+                  ? 'news-card-list__show-more-button news-card-list__show-more-button_hidden'
+                  : 'news-card-list__show-more-button'
+              }
+              type="button"
+              onClick={handleShowMoreClick}
+            >
+              Show more
+            </button>
+          </div>
+        </>
+      )}
     </section>
   );
 }
 
 SavedCardList.propTypes = {
   result: PropTypes.instanceOf(Object),
+  error: PropTypes.instanceOf(Object),
   thunkDispatch: PropTypes.func.isRequired,
 };
 
 SavedCardList.defaultProps = {
   result: [],
+  error: {},
 };
 
 export default SavedCardList;
--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -24,7 +24,7 @@ function SavedNews({ setIsHome }) {
       <Preloader isLoading={loading} />
       <main className={loading ? 'saved-news saved-news_hidden' : 'saved-news'}>
         <SavedNewsHeader result={result} />
-        <SavedCardList result={result} thunkDispatch={thunkDispatch}>
+        <SavedCardList result={result} error={error} thunkDispatch={thunkDispatch}>
           <div className="news-card__keyword">{}</div>
         </SavedCardList>
       </main>
