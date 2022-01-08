e8e42f68bb73f252540d4906788b7eb37bc864c6
Yaniv Schweitzer <yaniv10501@gmail.com>
2022-01-08 18:15:01 +0200

    clear search articles ids after logout

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -86,7 +86,7 @@ function NewsCardList({
         setCardAmount(2);
         loadingImages = [];
       }
-      console.log(result, result.articles);
+      console.log(result);
       if (result && result.articles) {
         if (loggedIn) {
           mainApi.checkSavedArticles(thunkDispatch, result.articles, false).then((response) => {
