8ef337145720f79b96974c29d6e9557ac0771408
Yaniv Schweitzer <yaniv10501@gmail.com>
2022-01-08 23:31:22 +0200

    Fix: nothing found

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -12,6 +12,7 @@ class NewsApi {
   searchArticles = (dispatch, query) =>
     useFetch(dispatch, `${this.baseUrl + query}&from=${this.from}&to=${this.to}`).then(
       (response) => {
+        console.log(response);
         if (response.articles && response.articles.length === 0) {
           dispatch({ type: 'NOTHING_FOUND' });
         }
