34435346dd83700b0be18d2968520f5722897abc
Yaniv Schweitzer <yaniv10501@gmail.com>
2022-01-08 15:52:34 +0200

    signUp form validation

--- src/utils/useFormValidation.js
+++ src/utils/useFormValidation.js
@@ -1,12 +1,27 @@
 import { useState, useCallback } from 'react';
+import { testStrength, testValid } from './regex';
 
 const useFormValidation = () => {
   const [values, setValues] = useState({});
   const [errors, setErrors] = useState({});
-  const [isValid, setIsValid] = useState(true);
+  const [isValid, setIsValid] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    if (name === 'password') {
+      const passwordInput = e.target;
+      const passwordValue = passwordInput.value;
+      if (passwordValue.length > 6) {
+        if (testValid(passwordValue)) {
+          if (!testStrength(passwordValue)) {
+            passwordInput.validationMessage =
+              'Please include at least 1 uppercase character, 1 lowercase character, and 1 number.';
+          }
+        } else {
+          passwordInput.validationMessage = 'Password is invalid';
+        }
+      }
+    }
     setValues({ ...values, [name]: value });
     setErrors({ ...errors, [name]: e.target.validationMessage });
     setIsValid(e.target.closest('form').checkValidity());
