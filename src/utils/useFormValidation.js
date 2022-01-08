14364e81bdafe6c332831dc13e669b1b280d21ae
Yaniv Schweitzer <yaniv10501@gmail.com>
2022-01-08 16:01:04 +0200

    signUp form validation

--- src/utils/useFormValidation.js
+++ src/utils/useFormValidation.js
@@ -6,12 +6,12 @@ const useFormValidation = () => {
   const [errors, setErrors] = useState({});
   const [isValid, setIsValid] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const handleChange = (event) => {
+    const { name, value } = event.target;
     if (name === 'password') {
-      const passwordInput = e.target;
+      const passwordInput = event.target;
       const passwordValue = passwordInput.value;
-      if (passwordValue.length > 6) {
+      if (passwordValue.length >= 6) {
         if (testValid(passwordValue)) {
           if (!testStrength(passwordValue)) {
             passwordInput.customMessage =
@@ -22,11 +22,26 @@ const useFormValidation = () => {
         } else {
           passwordInput.customMessage = 'Password is invalid';
         }
+      } else {
+        passwordInput.customMessage = '';
+      }
+    }
+    if (name === 'name') {
+      const nameInput = event.target;
+      const nameValue = nameInput.value;
+      if (nameValue.length >= 2 && nameValue.length <= 30) {
+        if (testValid(nameValue)) {
+          nameInput.customMessage = '';
+        } else {
+          nameInput.customMessage = 'Name is invalid';
+        }
+      } else {
+        nameInput.customMessage = '';
       }
     }
     setValues({ ...values, [name]: value });
-    setErrors({ ...errors, [name]: e.target.customMessage || e.target.validationMessage });
-    setIsValid(e.target.closest('form').checkValidity());
+    setErrors({ ...errors, [name]: event.target.customMessage || event.target.validationMessage });
+    setIsValid(event.target.closest('form').checkValidity());
   };
 
   const resetForm = useCallback(
